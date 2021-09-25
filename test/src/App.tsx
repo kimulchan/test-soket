0ee8c3811ac493bd3833e5dbf6f4d944ede36f70
kimulchan <leerabi577@gmail.com>
2021-09-25 23:31:50 +0900

    소켓 초기화

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,61 +1,14 @@
-import React, { ChangeEvent, FormEvent } from 'react';
-import './App.css';
-import socketIOClient from "socket.io-client";
+import React from "react";
+import { useEffect } from "react";
+import SocketIoClient from "socket.io-client";
 
-interface Message { name: string, text: string }
-const App: React.FC = () => {
-  const [messageList, setMessageList] = React.useState<Message[]>([]);
-  const [name, setName] = React.useState('');
-  const [value, setValue] = React.useState('');
-  const socket = socketIOClient('http://220.90.237.33:4300',{
-    transports: ['websocket'],
-    jsonp: false}
-  );
-  const submit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(name,value);
-    
-    socket.emit('msgToServer', { name: name, text: value },(error:any)=>{
-      console.log(error)
-    });
-  };
-  React.useEffect(() => {
-    socket.on('msgToClient', (message: { name: string, text: string }) => {
-      setMessageList(messageList => messageList.concat(message));
-    })
-  }, []);
-
+function App (){
+  const socket = SocketIoClient("http://220.90.237.33:4900")
+  
   return (
-    <div className="App">
-      <section className="chat-list">
-        {messageList.map((item: Message, i: number) =>
-          <div key={i} className="message">
-            <p className="username">{item.name.toUpperCase()}</p>
-            <p className="message-text">{item.text}</p>
-          </div>
-        )}
-      </section>
-      <form className="chat-form"
-        onSubmit={(e: FormEvent<HTMLFormElement>) => submit(e)}>
-        <div className="chat-inputs">
-          <input
-            type="text"
-            autoComplete="off"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-            value={name}
-            placeholder="유저이름"
-          />
-          <input
-            type="text"
-            autoComplete="off"
-            onChange={(e: ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-            value={value}
-            placeholder="메세지입력하기"
-          />
-        </div>
-        <button type="submit">입력하기</button>
-      </form>
-    </div>
+    <>
+
+    </>
   );
 }
 
